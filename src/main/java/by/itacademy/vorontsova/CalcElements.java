379af290d7c918f6b728685ecaacc1b1fb3ba7a2
Veronika <veronika.vorontsova1@gmail.com>
2023-11-16 03:07:09 +0300

    Created PageObject class, added additional fields validation

---
+++ src/main/java/by/itacademy/vorontsova/CalcElements.java
@@ -0,0 +1,14 @@
+package by.itacademy.vorontsova;
+
+public class CalcElements {
+
+    public String nameFieldLocator = "//input[@name='name']";
+    public String heightFieldLocator = "//input[@name='height']";
+    public String weightFieldLocator = "//input[@name='weight']";
+    public String genderMRadioLocator = "//input[@value='m']";
+    public String calculateButtonLocator = "//input[@value='Рассчитать']";
+    public String warningMessageEmptyFieldsLocator = "//tbody/tr[1]/td/b";
+    public String successMessageLocator = "/html/body/table/tbody/tr[2]/td[2]";
+
+
+}
--- src/test/java/by/itacademy/vorontsova/WeightCalcTest.java
+++ src/test/java/by/itacademy/vorontsova/WeightCalcTest.java
@@ -6,36 +6,128 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import static java.lang.Thread.sleep;
 
 
 public class WeightCalcTest {
-    @Test
+    @Test //Test1 - All fields are empty
     public void testFormWithEmptyValues() throws InterruptedException {
+        CalcElements calcElements = new CalcElements();
         WebDriver driver = new ChromeDriver();
         driver.get("https://svyatoslav.biz/testlab/wt/index.php");
 
-        // click "Calculate" button
-        WebElement calculateButtonWebElement = driver.findElement(By.xpath("//input[@value='Рассчитать']"));
-        calculateButtonWebElement.click();
-        
-        // get warming message text from WebElement
-        WebElement warningMessageEmptyFields = driver.findElement(By.xpath("//tbody/tr[1]/td/b"));
+        driver.findElement(By.xpath(calcElements.calculateButtonLocator)).click();
 
-        // set actual and expected results
-        String actual = warningMessageEmptyFields.getText();
+        String actual = driver.findElement(By.xpath(calcElements.warningMessageEmptyFieldsLocator)).getText();
         String expected = """
                 Не указано имя.
                 Рост должен быть в диапазоне 50-300 см.
                 Вес должен быть в диапазоне 3-500 кг.
                 Не указан пол.""";
 
-        //compare actual and expected results
         Assertions.assertEquals(expected, actual);
-
-        //quit browser
-        sleep(3000);
+        Thread.sleep(2000);
         driver.quit();
     }
+
+    @Test //Test2 - Name field is filled
+    public void testFormNameOnly() throws InterruptedException {
+        CalcElements calcElements = new CalcElements();
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://svyatoslav.biz/testlab/wt/index.php");
+
+        driver.findElement(By.xpath(calcElements.nameFieldLocator)).sendKeys("Steve");
+
+        driver.findElement(By.xpath(calcElements.calculateButtonLocator)).click();
+        Thread.sleep(1000);
+
+        String actual = driver.findElement(By.xpath(calcElements.warningMessageEmptyFieldsLocator)).getText();
+        Assertions.assertEquals("Рост должен быть в диапазоне 50-300 см.\n" + "Вес должен быть в диапазоне 3-500 кг.\n" + "Не указан пол.", actual);
+
+        Thread.sleep(2000);
+        driver.quit();
+    }
+
+    @Test  //Test3 - Name and height fields are filled
+    public void testFormNameAndHeight() throws InterruptedException {
+        CalcElements calcElements = new CalcElements();
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://svyatoslav.biz/testlab/wt/index.php");
+
+        WebElement nameField = driver.findElement(By.xpath(calcElements.nameFieldLocator));
+        nameField.sendKeys("Steve");
+
+        WebElement heightField = driver.findElement(By.xpath(calcElements.heightFieldLocator));
+        heightField.sendKeys("185");
+
+        WebElement calculateButtonWebElement = driver.findElement(By.xpath(calcElements.calculateButtonLocator));
+        calculateButtonWebElement.click();
+        Thread.sleep(1000);
+
+        WebElement warningMessageEmptyFields = driver.findElement(By.xpath(calcElements.warningMessageEmptyFieldsLocator));
+        String actual = warningMessageEmptyFields.getText();
+        Assertions.assertEquals("Вес должен быть в диапазоне 3-500 кг.\n" + "Не указан пол.", actual);
+
+        Thread.sleep(2000);
+        driver.quit();
+    }
+
+    @Test //Test4 - gender value is empty
+    public void testFormGenderEmpty() throws InterruptedException {
+        CalcElements calcElements = new CalcElements();
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://svyatoslav.biz/testlab/wt/index.php");
+
+        WebElement nameField = driver.findElement(By.xpath(calcElements.nameFieldLocator));
+        nameField.sendKeys("Steve");
+
+        WebElement heightField = driver.findElement(By.xpath(calcElements.heightFieldLocator));
+        heightField.sendKeys("185");
+
+        WebElement weightField = driver.findElement(By.xpath(calcElements.weightFieldLocator));
+        weightField.sendKeys("86");
+
+        WebElement calculateButtonWebElement = driver.findElement(By.xpath(calcElements.calculateButtonLocator));
+        calculateButtonWebElement.click();
+        Thread.sleep(1000);
+
+        WebElement warningMessageEmptyFields = driver.findElement(By.xpath(calcElements.warningMessageEmptyFieldsLocator));
+        String actual = warningMessageEmptyFields.getText();
+        Assertions.assertEquals("Не указан пол.", actual);
+
+        Thread.sleep(2000);
+        driver.quit();
+    }
+
+    @Test //Test5 - all values are filled correctly
+    public void testFormFieldsCorrect() throws InterruptedException {
+        CalcElements calcElements = new CalcElements();
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://svyatoslav.biz/testlab/wt/index.php");
+
+        WebElement nameField = driver.findElement(By.xpath(calcElements.nameFieldLocator));
+        nameField.sendKeys("Steve");
+
+        WebElement heightField = driver.findElement(By.xpath(calcElements.heightFieldLocator));
+        heightField.sendKeys("185");
+
+        WebElement weightField = driver.findElement(By.xpath(calcElements.weightFieldLocator));
+        weightField.sendKeys("86");
+
+        WebElement genderMradio = driver.findElement(By.xpath(calcElements.genderMRadioLocator));
+        genderMradio.click();
+
+        WebElement calculateButtonWebElement = driver.findElement(By.xpath(calcElements.calculateButtonLocator));
+        calculateButtonWebElement.click();
+        Thread.sleep(1000);
+
+        WebElement successMessage = driver.findElement(By.xpath(calcElements.successMessageLocator));
+        String actual = successMessage.getText();
+        Assertions.assertEquals("Идеальная масса тела", actual);
+
+        Thread.sleep(2000);
+        driver.quit();
+
+    }
+
 }
 
